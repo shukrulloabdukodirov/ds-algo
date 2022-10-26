3b6ce974ab4d6109fa0c273612d5097733132e99
Shukrullo Abduqodirov <abduqodirov0101@gmail.com>
2022-10-26 15:46:45 +0500

    Linked List opertions

--- Data structures/LinkedList/LinkedList.php
+++ Data structures/LinkedList/LinkedList.php
@@ -31,4 +31,152 @@ class LinkedList{
             $currrentNode = $currrentNode->next;
         }
     }
+
+    public function insertAtFirst(string $data = NULL) {
+        $newNode = new ListNode($data);
+        if ($this->_firstNode === NULL) {
+            $this->_firstNode = &$newNode;
+        } else {
+                $currentFirstNode = $this->_firstNode;
+                $this->_firstNode = &$newNode;
+                $newNode->next = $currentFirstNode;
+        } 
+        $this->_totalNode++;
+        return TRUE;
+    }
+
+    public function search(string $data = NULL) {
+        if ($this->_totalNode) {
+
+            $currentNode = $this->_firstNode;
+            while ($currentNode !== NULL) {
+                if($currentNode->data === $data) {
+                    return $currentNode;
+                }
+
+                $currentNode = $currentNode->next;
+            }
+        }
+        return FALSE;
+    }
+
+    public function insertBefore(string $data = NULL, string $query = NULL) {
+        $newNode = new ListNode($data);
+        if ($this->_firstNode) {
+            $previous = NULL;
+            $currentNode = $this->_firstNode;
+            while ($currentNode !== NULL) {
+                if ($currentNode->data === $query) {
+                    $newNode->next = $currentNode;
+                    $previous->next = $newNode;
+                    $this->_totalNode++;
+                    break;
+                }
+                $previous = $currentNode;
+                $currentNode = $currentNode->next;
+            }
+        }
+    }
+
+    public function insertAfter(string $data = NULL, string $query =  NULL) { 
+        $newNode = new ListNode($data);  
+        if ($this->_firstNode) {  
+            $nextNode= NULL;  
+            $currentNode = $this->_firstNode;  
+                while ($currentNode !== NULL){  
+                    if ($currentNode->data === $query) {  
+                        if($nextNode !== NULL) { 
+                        $newNode->next = $nextNode;  
+                    }  
+                    $currentNode->next = $newNode; 
+                    $this->_totalNode++;  
+                    break;  
+                }  
+                $currentNode = $currentNode->next;
+                $nextNode = $currentNode->next;  
+            }  
+        }  
+    }
+
+    public function deleteFirst() {
+        if ($this->_firstNode !== NULL) {
+            if ($this->_firstNode->next !== NULL) {
+                $this->_firstNode = $this->_firstNode->next;
+            } else {
+                $this->_firstNode = NULL;
+            }
+            $this->_totalNode--;
+            return TRUE;
+        }
+        return FALSE;
+    }
+
+    public function deleteLast() { 
+        if ($this->_firstNode !== NULL) {
+            $currentNode = $this->_firstNode; 
+            if ($currentNode->next === NULL) {
+                $this->_firstNode = NULL; 
+            } else { 
+                $previousNode = NULL;
+                while ($currentNode->next !== NULL) { 
+                    $previousNode = $currentNode;
+                    $currentNode = $currentNode->next; 
+                } 
+                $previousNode->next = NULL;
+                $this->_totalNode--; 
+                return TRUE; 
+            } 
+        } 
+        return FALSE;
+    }
+
+    public function delete(string $query = NULL) { 
+        if ($this->_firstNode) {
+            $previous = NULL; 
+            $currentNode = $this->_firstNode; 
+            while ($currentNode !== NULL) { 
+                if ($currentNode->data === $query) { 
+                    if ($currentNode->next === NULL) { 
+                        $previous->next = NULL; 
+                    } else { 
+                        $previous->next = $currentNode->next; 
+                    } $this->_totalNode--; 
+                    break; 
+                }
+                $previous = $currentNode; 
+                $currentNode = $currentNode->next; 
+            } 
+        }
+    }
+
+    public function reverse() {  
+        if ($this->_firstNode !== NULL) {  
+            if ($this->_firstNode->next !== NULL) {  
+                $reversedList = NULL;  $next = NULL; 
+                $currentNode = $this->_firstNode;  
+                while ($currentNode !== NULL) {  
+                    $next = $currentNode->next;  
+                    $currentNode->next = $reversedList;  
+                    $reversedList = $currentNode;
+                    $currentNode = $next;  
+                }  
+                $this->_firstNode =$reversedList;  
+            }  
+        }  
+    }
+
+    public function getNthNode(int $n = 0) {
+        $count = 1;
+        if ($this->_firstNode !== NULL) {
+            $currentNode = $this->_firstNode;
+            while ($currentNode !== NULL) {
+                if ($count === $n) {
+                    return $currentNode;
+                }
+                $count++;
+                $currentNode = $currentNode->next;
+            }
+        }
+    }
+        
 }
--- Data structures/LinkedList/index.php
+++ Data structures/LinkedList/index.php
@@ -5,6 +5,16 @@ $BookTitles = new LinkedList();
 $BookTitles->insert("Introduction to Algorithm");
 $BookTitles->insert("Introduction to PHP and Data structures");
 $BookTitles->insert("Programming Intelligence");
+$BookTitles->insertAtFirst("Mediawiki Administrative tutorial guide");
+$BookTitles->insertBefore("Introduction to Calculus", "Programming Intelligence");
+$BookTitles->insertAfter("Introduction to Calculus", "Programming Intelligence");
 $BookTitles->display();
+$BookTitles->deleteFirst();
+$BookTitles->deleteLast();
+$BookTitles->delete("Introduction to PHP and Data structures");
+$BookTitles->reverse();
+$BookTitles->display();
+echo "2nd Item is: ".$BookTitles->getNthNode(2)->data;
+
 
 ?>
